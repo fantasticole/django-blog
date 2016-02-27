58a0383358fa439e8c41b336265d0d1f54022aed
Cole Murphy <cole.murphy@venuebook.com>
2016-02-27 14:58:16 -0500

    Added views to create/edit blog post inside the site and update title field type.

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
 	author = models.ForeignKey('auth.User')
-	title = models.TextField()
+	title = models.CharField(max_length=200)
 	text = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
