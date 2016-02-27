96fae6e8292e994b7e44cc6e739066e7f403ef26
Cole Murphy <cole.murphy@venuebook.com>
2016-02-27 13:19:04 -0500

    Add missing text field.

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Post(models.Model):
 	author = models.ForeignKey('auth.User')
 	title = models.TextField()
+	text = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
 
